7b608a67bbf22c6640a3e7121e3900346143e2ea
Stephen Jue <stephenjue@Stephens-Air.home>
2022-08-12 22:01:17 -0400

    cleaned up code for index.html, server.ts, webpack

--- src/server/server.ts
+++ src/server/server.ts
@@ -9,11 +9,14 @@ const app = express();
 app.use(express.json());
 
 // serve routes
-// condition: NODE_ENV is production, serve the dist folder bundle js
-// serve index.html??? as well???
+// condition: NODE_ENV is production, serve static files
 if(process.env.NODE_ENV === 'production') {
   app.use('/', express.static(path.join(__dirname, '../../dist')));
 }
+
+// Is this redundant? WDS proxy pointed to root is redundant?
+// app.get('/', (req, res) => res.status(200).sendFile(path.join(__dirname, '../client/index.html')));
+
 // global error handler
 
 app.listen(3000, () => console.log('Server is listening on PORT 3000'));
